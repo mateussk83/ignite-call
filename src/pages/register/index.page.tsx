2878c430fe6f096031f4f53528280a9312eb0cea
mateussk83 <mateussk83@gmail.com>
2023-07-27 19:44:08 -0300

    insert cookie info userId

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,12 +6,14 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
     .string()
     .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
-    .regex(/^((a-z)\\-]+)$/i, {
+    .regex(/^([a-z\\-]+)$/i, {
       message: 'O usuário pode ter apenas letras e hifens',
     })
     .transform((username) => username.toLowerCase()),
@@ -40,7 +42,17 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+      }
+      console.error(err)
+    }
   }
   return (
     <Container>
